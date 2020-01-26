214b186bb767b7d51eb1e9ca2af06a021b636490
Abhay Maurya <maurya.abhay30@gmail.com>
2020-01-26 19:52:25 +0530

    Polynomials using Array (Add and Subtract 2 Polynomials)

---
+++ PolynomialsUsingArray.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+void cout_poly(int p[], int n)
+{
+    cout<<endl;
+    for(int i=0;i<=n;i++)
+    {
+       cout << p[i];
+       if (i != 0)
+        cout << "x^" << i ;
+       if (i != n)
+       cout << " + ";
+    }
+}
+
+void cin_poly(int p[], int n)
+{
+    cout<<"\nEnter the coefficients : "<<endl;
+    for(int i=0;i<=n;i++)
+    {
+        cin>>p[i];
+    }
+}
+
+void add_poly(int p1[], int m, int p2[], int n)
+{
+    int l=max(m,n);
+    int c[l];
+    for(int i=0;i<=l;i++)
+    {
+        c[i]=0;
+    }
+    for(int i=0;i<=m;i++)
+    {
+        c[i]=p1[i];
+    }
+    for(int i=0;i<=n;i++)
+    {
+        c[i]=c[i]+p2[i];
+    }
+    cout<<"\nAddition of both the polynomials : "<<endl;
+    cout_poly(c,l);
+}
+
+void sub_poly(int p1[], int m, int p2[], int n)
+{
+    int l=max(m,n);
+    int c[l];
+    for(int i=0;i<=l;i++)
+    {
+        c[i]=0;
+    }
+    for(int i=0;i<=m;i++)
+    {
+        c[i]=p1[i];
+    }
+    for(int i=0;i<=n;i++)
+    {
+        c[i]=c[i]-p2[i];
+    }
+    cout<<"\nSubtraction of both the polynomials : "<<endl;
+    cout_poly(c,l);
+}
+
+int main()
+{
+    int m,n;
+    cout<<"\nEnter first polynomial degree : "<<endl;
+    cin>>m;
+    int a[m];
+    cin_poly(a,m);
+    cout_poly(a,m);
+    cout<<"\nEnter second polynomial degree : "<<endl;
+    cin>>n;
+    int b[n];
+    cin_poly(b,n);
+    cout_poly(b,n);
+    add_poly(a,m,b,n);
+    sub_poly(a,m,b,n);
+}
