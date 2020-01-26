593abf0238e19a790526297fd472dec146f21b6e
Abhay Maurya <maurya.abhay30@gmail.com>
2020-01-26 20:04:09 +0530

    Added Multiplication of Polynomials

--- PolynomialsUsingArray.cpp
+++ PolynomialsUsingArray.cpp
@@ -63,6 +63,25 @@ void sub_poly(int p1[], int m, int p2[], int n)
     cout_poly(c,l);
 }
 
+void mul_poly(int p1[], int m, int p2[], int n)
+{
+    int l=m+n-1;
+    int c[l];
+    for(int i=0;i<=l;i++)
+    {
+        c[i]=0;
+    }
+    for (int i=0; i<m; i++)
+   {
+     for (int j=0; j<n; j++)
+     {
+         c[i+j] = c[i+j] p1[i]*p2[j];
+     }
+   }
+    cout<<"\nMultiplication of both the polynomials : "<<endl;
+    cout_poly(c,l);
+}
+
 int main()
 {
     int m,n;
@@ -78,4 +97,5 @@ int main()
     cout_poly(b,n);
     add_poly(a,m,b,n);
     sub_poly(a,m,b,n);
+    mul_poly(a,m,b,n);
 }
